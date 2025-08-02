43b065076f8db900e60931a4957bfebec6d1f243
kittichanr <petchkubbb@gmail.com>
2025-08-02 14:39:24 +0700

    Extract startup code to reuse both of main and test module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::net::TcpListener;
-
-use sqlx::PgPool;
 use zero2prod::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -16,24 +12,8 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
     println!("{configuration:#?}");
 
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.connection_options());
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
