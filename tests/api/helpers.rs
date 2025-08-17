cdb2c1811e831ceb01e6eee5365195d6d5da9f91
kittichanr <petchkubbb@gmail.com>
2025-08-17 10:12:30 +0700

    Handle reject unauth user

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -117,14 +117,15 @@ impl TestApp {
     }
 
     pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard().await.text().await.unwrap()
+    }
+
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(format!("{}/admin/dashboard", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
-            .text()
-            .await
-            .unwrap()
     }
 }
 
